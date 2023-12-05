2fa148f577016917ffdd1a76111fbd68369fbae5
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2023-12-05 11:01:40 -0800

    v1.0.0-ALPHA1

--- src/Terpz710/MotdPE/Main.php
+++ src/Terpz710/MotdPE/Main.php
@@ -13,6 +13,8 @@ use Terpz710\MotdPE\MotdTask;
 
 class Main extends PluginBase implements Listener {
 
+    private $subRotateInterval;
+
     public function onEnable(): void {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
 
@@ -21,13 +23,30 @@ class Main extends PluginBase implements Listener {
 
         if ($config->get("rotate_interval", true)) {
             $interval = $config->get("update_interval", 300);
-            $this->getScheduler()->scheduleRepeatingTask(new MotdTask($this, $config->get("motd_list", [])), $interval);
+            $this->subRotateInterval = $config->get("sub_rotate_interval", true);
+            $this->getScheduler()->scheduleRepeatingTask(
+                new MotdTask(
+                    $this,
+                    $config->get("motd_list", []),
+                    $config->get("sub_motd_list", []),
+                    $this->subRotateInterval,
+                    $config->get("sub_update_interval", 120)
+                ),
+                $interval
+            );
         } else {
-            $this->updateMOTD($config->get("static_motd", ""));
+            $this->updateMOTD(
+                $config->get("static_motd", ""),
+                $config->get("static_sub_motd", "")
+            );
         }
     }
 
-    public function updateMOTD(string $newMOTD) {
-        $this->getServer()->getNetwork()->setName($newMOTD);
+    public function updateMOTD(string $newMOTD, string $newSubMOTD = "") {
+        $network = $this->getServer()->getNetwork();
+        $network->setName($newMOTD);
+        if (!empty($newSubMOTD)) {
+            $network->setSubName($newSubMOTD);
+        }
     }
 }
