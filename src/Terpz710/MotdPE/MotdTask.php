f623641937865590b8c5508bef670d7dabee7179
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2023-12-05 11:02:52 -0800

    v1.0.0-ALPHA1

--- src/Terpz710/MotdPE/MotdTask.php
+++ src/Terpz710/MotdPE/MotdTask.php
@@ -10,14 +10,30 @@ class MotdTask extends Task {
 
     private $plugin;
     private $motdList;
+    private $subMotdList;
+    private $subRotateInterval;
+    private $subUpdateInterval;
+    private $subUpdateCounter = 0;
 
-    public function __construct(Main $plugin, array $motdList) {
+    public function __construct(Main $plugin, array $motdList, array $subMotdList = [], bool $subRotateInterval = false, int $subUpdateInterval = 300) {
         $this->plugin = $plugin;
         $this->motdList = $motdList;
+        $this->subMotdList = $subMotdList;
+        $this->subRotateInterval = $subRotateInterval;
+        $this->subUpdateInterval = $subUpdateInterval;
     }
 
     public function onRun(): void {
         $randomMOTD = $this->motdList[array_rand($this->motdList)];
+        if ($this->subRotateInterval && !empty($this->subMotdList)) {
+            $this->subUpdateCounter++;
+            if ($this->subUpdateCounter >= $this->subUpdateInterval) {
+                $randomSubMOTD = $this->subMotdList[array_rand($this->subMotdList)];
+                $this->plugin->updateMOTD($randomMOTD, $randomSubMOTD);
+                $this->subUpdateCounter = 0;
+                return;
+            }
+        }
         $this->plugin->updateMOTD($randomMOTD);
     }
 }
